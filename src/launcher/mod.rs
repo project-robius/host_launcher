40561b434f2ff6f3760a0197577e17cfad0f79c3
Kevin Boos <kevinaboos@gmail.com>
2026-07-30 18:47:57 -0700

    launcher: register the source, import and providers modules with the vm

--- src/launcher/mod.rs
+++ src/launcher/mod.rs
@@ -13,6 +13,9 @@ pub mod dock;
 pub mod notif_badge;
 pub mod search_overlay;
 pub mod app_info;
+pub mod import_modal;
+pub mod providers_page;
+pub mod source_modal;
 
 pub fn script_mod(vm: &mut ScriptVm) {
     // Order matters here, as some widget definitions depend on others.
@@ -22,7 +25,10 @@ pub fn script_mod(vm: &mut ScriptVm) {
     app_drawer::script_mod(vm);
     context_menu::script_mod(vm);
     app_info::script_mod(vm);
+    source_modal::script_mod(vm);
     app_store::script_mod(vm);
+    import_modal::script_mod(vm);
+    providers_page::script_mod(vm);
     dock::script_mod(vm);
     search_overlay::script_mod(vm);
     create_bar::script_mod(vm);
